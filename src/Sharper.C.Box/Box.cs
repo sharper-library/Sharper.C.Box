e2c92725f12bc7a2a2882c7d1a158f09421dae06
Jeremy Hughes <jeremy@tflite.com>
2015-07-21 14:30:50 +1200

    init

---
+++ src/Sharper.C.Box/Box.cs
@@ -0,0 +1,35 @@
+﻿namespace Sharper.C
+{
+    public sealed class Box<A>
+        where A : struct
+    {
+        internal Box(A a)
+        {
+            Unbox = a;
+        }
+
+        public A Unbox { get; }
+
+        public bool Equals(Box<A> ba)
+            => ReferenceEquals(this, ba) || Unbox.Equals(ba.Unbox);
+
+        public override bool Equals(object o)
+            => !ReferenceEquals(null, o)
+                && o is Box<A>
+                && Equals((Box<A>) o);
+
+        public override int GetHashCode()
+        {
+            return Unbox.GetHashCode();
+        }
+
+        public override string ToString() => $"{nameof(Box<A>)}({Unbox})";
+    }
+
+    public static class Box
+    {
+        public static Box<A> Mk<A>(A a)
+            where A : struct
+            => new Box<A>(a);
+    }
+}

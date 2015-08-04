bbb7b2d0bb39a6635a557315a5b3839e3464e8af
Jeremy Hughes <jeremy@tflite.com>
2015-08-04 13:07:18 +1200

    Add mutable box type

---
+++ Sharper.C.Box/Unsafe/MBox.cs
@@ -0,0 +1,18 @@
+﻿namespace Sharper.C.Data.Unsafe
+{
+    public sealed class MBox<A>
+    {
+        internal MBox(A a)
+        {
+            Unbox = a;
+        }
+
+        public A Unbox { get; set; }
+    }
+
+    public static class MBox
+    {
+        public static MBox<A> Mk<A>(A a)
+            => new MBox<A>(a);
+    }
+}
